c5f9105ff442889a5d6c36235e6917aaf79a3849
YoonkunLee <ydx9474@aut.ac.nz>
2020-10-21 21:16:55 +1300

    First Commit

---
+++ VehicleRegister/VehicleRegister.Data.MSSQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleRegister.Data.MSSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ VehicleRegister/VehicleRegister.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VehicleRegister.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
